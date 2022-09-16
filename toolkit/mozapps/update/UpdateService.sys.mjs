d7639e538ae03cfd45877c961f4b693247cd0759
Alex Kontos <MrAlex94@users.noreply.github.com>
2022-09-16 15:41:24 +0100

    refactor: rename Firefox related references to their relevant counterparts
    (cherry picked from commit 92becbe26d69a16f9e6f0b16fd2e949bc2db3ec3)

--- toolkit/mozapps/update/UpdateService.sys.mjs
+++ toolkit/mozapps/update/UpdateService.sys.mjs
@@ -1137,7 +1137,7 @@ function isServiceInstalled() {
     );
     wrk.open(
       wrk.ROOT_KEY_LOCAL_MACHINE,
-      "SOFTWARE\\Mozilla\\MaintenanceService",
+      "Software\\Waterfox\\MaintenanceService",
       wrk.ACCESS_READ | wrk.WOW64_64
     );
     installed = wrk.readIntValue("Installed");
@@ -5232,7 +5232,7 @@ export class CheckerService {
     );
 
     let regPath =
-      "SOFTWARE\\Mozilla\\" + Services.appinfo.name + "\\32to64DidMigrate";
+      "SOFTWARE\\BrowserWorks\\" + Services.appinfo.name + "\\32to64DidMigrate";
     let regValHKCU = lazy.WindowsRegistry.readRegKey(
       wrk.ROOT_KEY_CURRENT_USER,
       regPath,
