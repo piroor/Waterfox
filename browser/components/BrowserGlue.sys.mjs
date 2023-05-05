3eb84fd860624888e23cc76a4db2aa847d551bff
Alex Kontos <MrAlex94@users.noreply.github.com>
2023-05-05 13:38:39 +0100

    refactor: lazy load Waterfox components
    (cherry picked from commit 2f24e730cf3dbe38c935b37c47a821e03564be4d)

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -265,7 +265,7 @@ BrowserGlue.prototype = {
         const { BootstrapLoader } = ChromeUtils.import(
           "resource:///modules/BootstrapLoader.jsm"
         );
-        AddonManager.addExternalExtensionLoader(BootstrapLoader);
+        lazy.AddonManager.addExternalExtensionLoader(BootstrapLoader);
       break;
       case "notifications-open-settings":
         this._openPreferences("privacy-permissions");
@@ -466,7 +466,7 @@ BrowserGlue.prototype = {
       resolve => (this._firstWindowLoaded = resolve)
     );
 
-    WaterfoxGlue.init();
+    lazy.WaterfoxGlue.init();
   },
 
   // cleanup (called on application shutdown)
