2359f1b83e0c419cad0cb87df8c606896033bcc4
Alex Kontos <MrAlex94@users.noreply.github.com>
2023-09-29 11:24:25 +0100

    feat: add double-click select all behavior to urlbar
    (cherry picked from commit f893df9474ab1f15d40bb068f482a02e984e43ec)

--- browser/components/urlbar/UrlbarInput.sys.mjs
+++ browser/components/urlbar/UrlbarInput.sys.mjs
@@ -4032,16 +4032,20 @@ export class UrlbarInput {
 
   /**
    * Determines if we should select all the text in the Urlbar based on the
-   *  Urlbar state, and whether the selection is empty.
+   * clickSelectsAll pref, Urlbar state, and whether the selection is empty.
+   *
+   * @param {boolean} [ignoreClickSelectsAllPref]
+   *        If true, the browser.urlbar.clickSelectsAll pref will be ignored.
    */
-  _maybeSelectAll() {
+  _maybeSelectAll(ignoreClickSelectsAllPref = false) {
     if (
       !this._preventClickSelectsAll &&
+      (ignoreClickSelectsAllPref || lazy.UrlbarPrefs.get("clickSelectsAll")) &&
       this._compositionState != lazy.UrlbarUtils.COMPOSITION.COMPOSING &&
       this.focused &&
       this.inputField.selectionStart == this.inputField.selectionEnd
     ) {
-      this.select();
+      this.editor.selectAll();
     }
   }
 
@@ -4169,7 +4173,9 @@ export class UrlbarInput {
       return;
     }
 
-    this._maybeSelectAll();
+    // If the user right clicks, we select all regardless of the value of
+    // the browser.urlbar.clickSelectsAll pref.
+    this._maybeSelectAll(/* ignoreClickSelectsAllPref */ event.button == 2);
   }
 
   _on_focus(event) {
@@ -4218,7 +4224,7 @@ export class UrlbarInput {
       }
 
       if (this.inputField.hasAttribute("refocused-by-panel")) {
-        this._maybeSelectAll();
+        this._maybeSelectAll(true);
       }
     }
 
@@ -4280,7 +4286,10 @@ export class UrlbarInput {
           this.inputField.setSelectionRange(0, 0);
         }
 
-        if (event.target.classList.contains(SEARCH_BUTTON_CLASS)) {
+        if (event.detail == 2 && lazy.UrlbarPrefs.get("doubleClickSelectsAll")) {
+          this.editor.selectAll();
+          event.preventDefault();
+        } else if (event.target.classList.contains(SEARCH_BUTTON_CLASS)) {
           this._preventClickSelectsAll = true;
           this.search(lazy.UrlbarTokenizer.RESTRICT.SEARCH);
         } else {
