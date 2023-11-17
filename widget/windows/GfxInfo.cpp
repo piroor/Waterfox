a999d1b21f699ab34653f935261b86d64e2d26b4
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2023-11-17 11:55:08 -0600

    feat: allow hardware acceleration in VMware on Windows
    (cherry picked from commit dd5a1ea020d3c8eb4adac1667a4712f242766e46)

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -1970,6 +1970,9 @@ nsresult GfxInfo::GetFeatureStatusImpl(
         !adapterVendorID.Equals(
             GfxDriverInfo::GetDeviceVendor(DeviceVendor::Qualcomm),
             nsCaseInsensitiveStringComparator) &&
+        !adapterVendorID.Equals(
+            GfxDriverInfo::GetDeviceVendor(DeviceVendor::VMWare),
+            nsCaseInsensitiveStringComparator) &&
         // FIXME - these special hex values are currently used in xpcshell tests
         // introduced by bug 625160 patch 8/8. Maybe these tests need to be
         // adjusted now that we're only whitelisting intel/ati/nvidia.
@@ -1979,9 +1982,6 @@ nsresult GfxInfo::GetFeatureStatusImpl(
         !adapterVendorID.LowerCaseEqualsLiteral("0xdcdc")) {
       if (adapterVendorID.Equals(
               GfxDriverInfo::GetDeviceVendor(DeviceVendor::MicrosoftHyperV),
-              nsCaseInsensitiveStringComparator) ||
-          adapterVendorID.Equals(
-              GfxDriverInfo::GetDeviceVendor(DeviceVendor::VMWare),
               nsCaseInsensitiveStringComparator) ||
           adapterVendorID.Equals(
               GfxDriverInfo::GetDeviceVendor(DeviceVendor::VirtualBox),
