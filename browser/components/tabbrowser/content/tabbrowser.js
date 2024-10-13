2fe037b702685631ef7278608f11b629a5b513c2
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2024-10-13 03:26:27 -0500

    feat: restore "unread" tab Attribute
    (cherry picked from commit 099b94371ced6363241e0ed5d9478c934405d362)

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -1303,6 +1303,7 @@
       if (!this._previewMode) {
         newTab.recordTimeFromUnloadToReload();
         newTab.updateLastAccessed();
+        newTab.removeAttribute("unread");
         oldTab.updateLastAccessed();
         // if this is the foreground window, update the last-seen timestamps.
         if (this.ownerGlobal == BrowserWindowTracker.getTopWindow()) {
@@ -8100,6 +8101,9 @@
 
             this.mTab.setAttribute("bursting", "true");
           }
+
+          if (!this.mTab.selected)
+            this.mTab.setAttribute("unread", "true");
         }
 
         if (this.mTab.hasAttribute("progress")) {
